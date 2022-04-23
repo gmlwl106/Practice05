21087fe67c49d607f02ad15d93ad119d6f676c60
gmlwl106 <66778894@naver.com>
2022-04-23 17:35:00 +0900

    Practice05. Ex07-FriendApp

--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -12,13 +12,32 @@ public class FriendApp {
         System.out.println("친구를 3명 등록해 주세요");
 
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
+        for(int i=0; i<friendArray.length; i++) {
+        	friendArray[i] = new Friend();
+        	
+        	System.out.print("이름: ");
+        	String name = sc.nextLine();
+        	friendArray[i].setName(name);
+        	
+        	System.out.print("핸드폰: ");
+        	String hp = sc.nextLine();
+        	friendArray[i].setHp(hp);
+        	
+        	System.out.print("학교: ");
+        	String school = sc.nextLine();
+        	friendArray[i].setSchool(school);
+        	
+        	System.out.println("-------------------------");
+        	
+        	
+        }
         
 
         
         // 친구정보 출력
         for (int i = 0; i < friendArray.length; i++) {
             //친구정보 출력 메소드 호출
+        	friendArray[i].showInfo();
         }
 
         sc.close();
