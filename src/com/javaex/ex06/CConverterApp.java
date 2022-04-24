7d4babbebe04401d1aab7ee0e52abedfb47812d9
gmlwl106 <66778894@naver.com>
2022-04-24 13:33:51 +0900

    Practice05. Ex06-CConverterApp(static)

--- src/com/javaex/ex06/CConverterApp.java
+++ src/com/javaex/ex06/CConverterApp.java
@@ -11,10 +11,12 @@ public class CConverterApp {
         CConverter.setRate(1118.70);
         
         //100만원을 달러로 출력하기
-        System.out.println("백만원은 " + CConverter.toDoller(1000000) + "달러입니다.");
+        won = 1000000;
+        System.out.println("백만원은 " + CConverter.toDoller(won) + "달러입니다.");
         
         //100달려를 원으로 출력하기
-        System.out.println("백달러는 " + CConverter.toKWR(100) + "입니다.");
+        dollar = 100;
+        System.out.println("백달러는 " + CConverter.toKWR(dollar) + "입니다.");
         
     }
 
